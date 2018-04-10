04ad708b4a41fb4cccb4934f2567f7d9617b7ef5
benoummi <soufianebenoummi@gmail.com>
2018-04-10 18:43:01 +0100

    one page update password and edit profile

---
+++ src/AppBundle/Action/CurrentUserAction.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Instan't App project.
+ *
+ * (c) Instan't App <contact@instant-app.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace AppBundle\Action;
+
+
+class CurrentUserAction
+{
+
+}
