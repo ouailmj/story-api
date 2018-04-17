90a0ea94e7a1000ba1b3f52b9a9ca6e6d61b8fe6
Farah Oubelkas <farah.oubelkas@gmail.com>
2018-04-17 09:44:29 +0100

    tests structure

---
+++ tests/_support/Pages/login.php
@@ -0,0 +1,30 @@
+<?php
+namespace Page;
+
+class Login
+{
+    // include url of current page
+    public static $URL = '/auth/login';
+
+    /**
+     * Declare UI map for this page here. CSS or XPath allowed.
+     * public static $usernameField = '#username';
+     * public static $formSubmitButton = "#mainForm input[type=submit]";
+     */
+
+    public static $usernameField    = '_username';
+    public static $passwordField    = '_password';
+    public static $submitButton     = '_submit';
+
+    /**
+     * Basic route example for your current URL
+     * You can append any additional parameter to URL
+     * and use it in tests like: Page\Edit::route('/123-post');
+     */
+    public static function route($param)
+    {
+        return static::$URL.$param;
+    }
+
+
+}
