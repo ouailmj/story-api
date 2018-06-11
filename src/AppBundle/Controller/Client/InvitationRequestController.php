aebd4712296006867adfc74783f18e6de4690777
abdenbiworks <abdenbi.ou@outlook.com>
2018-06-11 14:30:02 +0000

    Event Invitations

--- src/AppBundle/Controller/Client/InvitationRequestController.php
+++ src/AppBundle/Controller/Client/InvitationRequestController.php
@@ -41,9 +41,7 @@ class InvitationRequestController extends BaseController
        }
        $acceptedInvitations =  $invitationRequestManager->getInvitationRequestByStatus($this->getUser(), false, true);
        $canceledInvitations =  $invitationRequestManager->getInvitationRequestByStatus($this->getUser(), true, false);
-        dump($this->getUser()->getEventMemberShips());
-        dump($acceptedInvitations);
-        dump($canceledInvitations);
+       
        return $this->render('client/invitation/index.html.twig', [
            'newInvitations' => $newInvitations,
            'invitationForms' => $invitationForms,
